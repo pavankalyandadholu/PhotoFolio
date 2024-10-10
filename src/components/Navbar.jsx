3eb482c58c96315e5fcdcf3cfc12e89e08bbf52a
Officerdhruva <dpavankalyan8887@gmail.com>
2024-10-10 20:40:17 +0530

    Complete Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (<>
+     <section className="w-full p-3 bg-gray-400">
+      <a href="/" className=" text-lg flex items-center gap-3">
+        <img className=" w-16" src="https://mellow-seahorse-fc9268.netlify.app/assets/logo.png" alt=""  /> <h3 >PhotoPolio</h3>
+         </a>
+    </section>
+    </>)
+};
